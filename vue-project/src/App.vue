76d365277342ddb1be4f35b62dfe3e05ff975f3b
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-01-25 15:34:01 +0800

    llll

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,57 @@
 <template>
-  <h1>Food</h1>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/> <br>
-  <personal-profile/> <br>
-  
+  <header>
+    Christina Prado - Personal Profile
+  </header>
+
+  <nav>
+    <a href="#about">About Me</a>
+    <a href="#education">Education</a>
+    <a href="#experience">IT Experiences</a>
+    <a href="#hobbies">Hobbies</a>
+    <a href="#goals">Goals</a>
+    <a href="#resources">Resources</a>
+  </nav>
+
+  <div class="container">
+    <div id="about" class="card">
+      <h2>About Me</h2>
+      <p>Hi! My name is Christina Prado, I am a second-year college student in Asia Pacific College taking the course BSIT-MI. I have five dogs and I love listening to music.</p>
+    </div>
+
+    <div id="education" class="card">
+      <h2>Education & Achievements</h2>
+      <p>BSIT-MI, Bachelor of Science in Information Technology with specialization in Mobile and Internet Technologies</p>
+    </div>
+
+    <div id="experience" class="card">
+      <h2>IT Experiences</h2>
+      <p>I first learned how to code using HTML when I was in 9th grade. And I kind've liked it, and that encouraged me to take IT.</p>
+    </div>
+
+    <div id="hobbies" class="card">
+      <h2>Hobbies & Interests</h2>
+      <p>I enjoy exploring new places through walking and eating out with friends. I find satisfaction and peace in a clean and organized room, and spending quality time with my beloved furbabies brings me immense joy.</p>
+    </div>
+
+    <div id="goals" class="card">
+      <h2>Goals in Life/Dreams</h2>
+      <p>As I look towards the future, my aspirations are to one day establish my own business, embark on adventures around the world, and to live a long, healthy, and stress-free life.</p>
+    </div>
+  </div>
+
+  <footer>Asia Pacific College</footer>
 </template>
 
-<script></script>
-
-<style>
-</style>           
+<style scoped>
+  body {
+    font-family: sans-serif;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-attachment: fixed;
+    background-color: #ffb5c0; 
+  }
+</style>
