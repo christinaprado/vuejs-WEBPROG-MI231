16722f3e6ed94282475ff706d053b38e8d7df9dc
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-01-25 15:44:48 +0800

    cgoakwdvcoquwg

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <h1>Food</h1>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/> <br>
+  <personal-profile/> <br>
   <div>
     <header>
       Christina Prado - Personal Profile
