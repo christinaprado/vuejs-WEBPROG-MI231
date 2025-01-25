7f6575585dca5ced6eb0532f116e3096d645e8f9
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-01-25 15:52:25 +0800

    cjfg

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,10 +1,4 @@
 <template>
-    <h1>Food</h1>
-    <food-item/> <br>
-    <food-item2/> <br>
-    <food-item/> <br>
-    <personal-profile/> <br>
-    <div>
       <header>
         Christina Prado - Personal Profile
       </header>
