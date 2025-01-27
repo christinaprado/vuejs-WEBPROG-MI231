60afd98108f84056d3fc04c04386cabb7f98d874
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-01-27 09:59:23 +0000

    trying

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,50 +1,31 @@
 <template>
-  <div class="container">
-    <ul>
+  <ul>
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
     </ul>
-
-    <div class="content-section">
-      <food-item />
-      <food-item2 />
-      <food-item3 />
-    </div>
-
-    <personal-profile />
-  </div>
+  <h1>Food</h1>
+  <personal-profile/>
+  <food-item/>
+  <food-item2/>
+  <food-item3/>
+  
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
 
-const countries = ref([])
+  const countries = ref([])
 
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
 
-onMounted(() => {
-  getCountries()
-})
-</script>
+  onMounted(() => {
+    getCountries()
+  })
+  
+  </script>
 
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.content-section {
-  display: flex;
-  justify-content: space-between;
-  width: 80%; 
-  margin-bottom: 20px; 
-}
-
-.container * {
-  color: white;
-}
-</style>
+<style>
+</style>   
