4aa2f63614165b6b44e41b0275df462fef11351f
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-01-27 09:30:10 +0000

    new added supabase again

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,14 +3,12 @@
     <ul>
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
     </ul>
-
-    <div class="content-section">
-      <food-item />
-      <food-item2 />
-      <food-item3 />
+    <div class="content">
+      <FoodItem />
+      <FoodItem2 />
+      <FoodItem3 />
     </div>
-
-    <personal-profile />
+    <PersonalProfile />
   </div>
 </template>
 
@@ -20,31 +18,28 @@
   flex-direction: column;
   align-items: center;
 }
-
-.content-section {
+.content {
   display: flex;
   justify-content: space-between;
-  width: 80%; 
-  margin-bottom: 20px; 
-}
-
-.container * {
-  color: white;
+  width: 80%;
+  margin-bottom: 1rem;
 }
 </style>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import FoodItem3 from './components/FoodItem3.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 
 const countries = ref([])
 
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
 onMounted(() => {
-  getCountries()
+  supabase
+    .from('countries')
+    .select()
+    .then(({ data }) => (countries.value = data))
 })
 </script>
