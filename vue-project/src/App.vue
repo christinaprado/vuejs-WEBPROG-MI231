66369b3ba18bb2408d6cf4dbb723ddaed9125e5b
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-04 09:55:21 +0000

    efgfmgh

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="container"> 
-    <ul> 
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li> 
-    </ul> 
+  <country/>
 
     <div class="content-section"> 
       <food-item /> 
@@ -11,24 +8,7 @@
     </div> 
 
     <personal-profile /> 
-  </div> 
 </template>
-
-<script setup> 
- import { ref, onMounted } from 'vue' 
- import { supabase } from './lib/supabaseClient' 
-
- const countries = ref([]) 
-
- async function getCountries() { 
-   const { data } = await supabase.from('countries').select() 
-   countries.value = data 
- } 
-
- onMounted(() => { 
-   getCountries() 
- }) 
-</script> 
 
 <style scoped> 
  .container { 
---
+++ vue-project/src/components/Country.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container"> 
+    <ul> 
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li> 
+    </ul> 
+    </div>
+</template>
+
+<script setup> 
+ import { ref, onMounted } from 'vue' 
+ import { supabase } from './lib/supabaseClient' 
+
+ const countries = ref([]) 
+
+ async function getCountries() { 
+   const { data } = await supabase.from('countries').select() 
+   countries.value = data 
+ } 
+
+ onMounted(() => { 
+   getCountries() 
+ }) 
+</script> 
