8d86d166e073d5b2f4dcf74a25c0758c1269893d
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-09 12:33:21 +0000

    zzzzz

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
-import FoodItem3 from './components/FoodItem3.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 import Country from './components/Country.vue'
 
@@ -11,7 +9,6 @@ const app = createApp(App)
 
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-app.component('food-item3', FoodItem3)
 app.component('personal-profile', PersonalProfile)
 app.component('country', Country)
 
