c8a3eef7b34af675d4f291fe34c09191df62392b
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-10 15:04:57 +0000

    v

---
+++ vue-project/src/components/Country.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Food</h1>
+    <ul>
+        <li v-for="country in country" :key="country.id">{{ country.name }}</li>
+      </ul>
+     <br>
+    <food-item/> 
+    <food-item2/> 
+    <food-item3/> 
+    <personal-profile/> <br>
+    
+  </template>
+  
+  <script setup>
+    import { ref, onMounted } from 'vue'
+    import { supabase } from './lib/supabaseClient'
+  
+    const Country = ref([])
+  
+    async function getCountry() {
+      const { data } = await supabase.from('country').select()
+      country.value = data
+    }
+  
+    onMounted(() => {
+      getCountry()
+    })
+    </script>
+  
+  
+  
+  
+  <style>
+  </style> 
