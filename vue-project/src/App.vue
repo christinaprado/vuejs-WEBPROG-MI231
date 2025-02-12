278fee0b217738fe3111338c6d8e598e81512b0c
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-12 14:07:54 +0000

    added countries

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <h1>Food</h1>
+  <h1>Food</h1>  
   <ul>
-      <li v-for="country in country" :key="country.id">{{ country.name }}</li>
-    </ul>
-   <br>
-  <food-item/> 
-  <food-item2/> 
-  <food-item3/> 
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+  </ul>
+  <br>
+  <food-item/>
+  <food-item2/>
+  <food-item3/>
   <personal-profile/> <br>
-  
 </template>
 
 <script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient.js'
+import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
+import FoodItem3 from './components/FoodItem3.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 
-  const Country = ref([])
+const countries = ref([]) // Use 'countries' for the ref variable (plural)
 
-  async function getCountry() {
-    const { data } = await supabase.from('country').select()
-    country.value = data
-  }
+async function getCountries() {
+  const { data } = await supabase.from('country').select() // 'country' is the table name
+  countries.value = data
+}
 
-  onMounted(() => {
-    getCountry()
-  })
-  </script>
-
-
-
+onMounted(() => {
+  getCountries()
+})
+</script>
 
 <style>
-</style> 
+</style>
