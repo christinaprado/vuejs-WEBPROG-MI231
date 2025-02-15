fe49e9aaf37fa91a95c2c0500f783c4bdd5e200b
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-16 03:56:18 +0800

    fixes

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -10,28 +10,4 @@
 
 </template>
 
-<script setup>
-const countries = ref([]) // Use 'countries' for the ref variable (plural)
-
-async function getCountries() {
-  const { data } = await supabase.from('country').select() // 'country' is the table name
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-
-const comments = ref([]) // Use 'countries' for the ref variable (plural)
-
-async function getComments() {
-  const { data } = await supabase.from('comment').select() // 'country' is the table name
-  comments.value = data
-}
-
-onMounted(() => {
-  getComments()
-})
-
-
-</script>
+<script></script>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -6,7 +6,7 @@ import FoodItem2 from './components/FoodItem2.vue'
 import FoodItem3 from './components/FoodItem3.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
 import Comment from './components/Comment.vue'
-import Commentform from './components/CommentForm.vue'
+import Commentform from './components/CommentForm.vue'  // Wrong casing
 
 const app = createApp(App)
 
@@ -15,6 +15,6 @@ app.component('food-item2', FoodItem2)
 app.component('food-item3', FoodItem3)
 app.component('personal-profile', PersonalProfile)
 app.component('comment', Comment)
-app.component('comment-form', CommentForm)
+app.component('comment-form', CommentForm) // `CommentForm` is undefined
 
 app.mount('#app')
