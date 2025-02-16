bde4846ed116f15c832f8f6b37f67c24ab26c05d
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-16 21:33:15 +0800

    shgvxcsghdvcjs

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <personal-profile/>
-  <country/>
+   <country/>
   <food-item/>
   <food-item2/>
   <food-item3/>
+  <personal-profile/>
   <comment-form/>
   <comment/>
    
@@ -11,11 +11,11 @@
 </template>
 
 <script setup>
-import PersonalProfile from './components/PersonalProfile.vue'
 import Country from './components/Country.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import FoodItem3 from './components/FoodItem3.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 </script>
