2473282b4079c2813457f7a7b178fc72b05f13a4
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-16 22:12:05 +0800

    dfvsefv

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,21 +1,21 @@
 <template>
+  <comment-form/>
+  <comment/>
    <country/>
   <food-item/>
   <food-item2/>
   <food-item3/>
   <personal-profile/>
-  <comment-form/>
-  <comment/>
-   
+  
 
 </template>
 
 <script setup>
-import PersonalProfile from './components/PersonalProfile.vue'
+import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 import Country from './components/Country.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import FoodItem3 from './components/FoodItem3.vue'
-import CommentForm from './components/CommentForm.vue'
-import Comment from './components/Comment.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 </script>
