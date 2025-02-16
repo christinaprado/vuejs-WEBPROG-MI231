a9c5c0a5122d986a9500d714ed48352d3f372c62
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-16 20:23:21 +0800

    rgwt4

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,21 +1,11 @@
 <template>
   <h1>Comments</h1>
   <ul>
-    <li v-for="comment in comments" :key="comment.id">{{ comment.name }} {{ comment.comment }}</li>
+    <li v-for="comment in comments" :key="comment.id">
+      {{ comment.name }}: {{ comment.comment }}
+    </li>
   </ul>
 </template>
-
-<script></script>
-
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    
-  }
-</style>
 
 <script setup>
 import { ref, onMounted } from 'vue'
@@ -24,23 +14,20 @@ import { supabase } from '@/lib/supabaseClient.js'
 const comments = ref([])
 
 async function getComments() {
-const { data } = await supabase.from('comments').select()
-comments.value = data
+  const { data } = await supabase.from('comments').select()
+  comments.value = data
 }
 
-onMounted(() => {
-getComments()
-})
-
+onMounted(getComments)
 </script>
 
-
 <style>
+/* Apply styles to comment boxes */
 #app > div {
   border: dashed black 1px;
   display: inline-block;
   margin: 10px;
   padding: 10px;
-  background-color: lightyellow;
+  background-color: #800020;
 }
 </style>
