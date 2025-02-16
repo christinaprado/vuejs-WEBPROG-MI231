3ede06bfd286ef940a2233e68da12a1ada5a1abf
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-16 21:45:09 +0800

    wdgveg

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -25,27 +25,9 @@ onMounted(getComments)
 /* Apply styles to comment boxes */
 #app > div {
   border: dashed black 1px;
-  display: inline-block; /* Key change: Make them inline */
+  display: inline-block;
   margin: 10px;
   padding: 10px;
   background-color: lightcoral;
-}
-
-/* New styles for horizontal alignment */
-#app {  /* Target the main app container */
-  display: flex;             /* Enable flexbox */
-  flex-wrap: wrap;         /* Allow wrapping to new lines if necessary */
-  justify-content: center; /* Center horizontally */
-  align-items: flex-start;  /* Align items to the top of the container */
-}
-
-ul {
-  list-style-type: none; /* Remove bullet points */
-  padding: 0;           /* Remove default padding */
-  margin: 0;            /* Remove default margins */
-}
-
-li {
-  margin-bottom: 5px;   /* Add some spacing between list items */
 }
 </style>
