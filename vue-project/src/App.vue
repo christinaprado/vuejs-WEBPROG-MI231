23af3ea0837bdd2dc92b2ad8625ed9e562611244
christinaprado <158535437+christinaprado@users.noreply.github.com>
2025-02-16 15:26:46 +0800

    added profile

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -15,6 +15,7 @@ import Country from './components/Country.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import FoodItem3 from './components/FoodItem3.vue'
+import PersonalProfile from './components/PersonalProfile.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 </script>
